b28862430fae69f0fc60503fb2f9e23d10b1e9c0
daemonprocellae@live.it <daemonprocellae@live.it>
2022-02-24 16:34:18 +0100

    Aggiunti cicli immagini

--- js/vueapp.js
+++ js/vueapp.js
@@ -1,33 +1,38 @@
-const slider = new Vue (
+const vueSlider = new Vue (
     {
-        el: ".section",
+        el: "#carousel-wrapper",
         data: {
-            slides = [
+            slides : [
             {
-                photo: 'img/01.jpg',
+                photo: './img/01.jpg',
                 country: 'Svezia',
                 caption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
             }, 
             {
-                photo: 'img/02.jpg',
+                photo: './img/02.jpg',
                 country: 'Svizzera',
                 caption: 'Lorem ipsum'
             },
             {
-                photo: 'img/03.jpg',
+                photo: './img/03.jpg',
                 country: 'Gran Bretagna',
                 caption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
             },
             {
-                photo: 'img/04.jpg',
+                photo: './img/04.jpg',
                 country: 'Germania',
                 caption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
             }, 
             {
-                photo: 'img/05.jpg',
+                photo: './img/05.jpg',
                 country: 'Paradise',
                 caption: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
             }],
-            idList: "img-wrapper miniatures up down"
+            idList: "img-wrapper miniatures up down arrows",
+            classList: "img-lg info title caption lil-img active active-xs"
+        },
+        methods: {
+
         }
     })
+
