83c51fe482abb46362fdc78d01504f5f397e4512
daemonprocellae@live.it <daemonprocellae@live.it>
2022-02-24 18:00:44 +0100

    Aggiunto active

--- js/vueapp.js
+++ js/vueapp.js
@@ -4,35 +4,44 @@ const vueSlider = new Vue (
         data: {
             slides : [
             {
-                photo: './img/01.jpg',
+                photo: 'img/01.jpg',
                 country: 'Svezia',
-                caption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                caption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                active: false
             }, 
             {
-                photo: './img/02.jpg',
+                photo: 'img/02.jpg',
                 country: 'Svizzera',
-                caption: 'Lorem ipsum'
+                caption: 'Lorem ipsum',
+                active: false
             },
             {
-                photo: './img/03.jpg',
+                photo: 'img/03.jpg',
                 country: 'Gran Bretagna',
                 caption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                active: true
             },
             {
-                photo: './img/04.jpg',
+                photo: 'img/04.jpg',
                 country: 'Germania',
                 caption: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                active: false
             }, 
             {
-                photo: './img/05.jpg',
+                photo: 'img/05.jpg',
                 country: 'Paradise',
-                caption: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                caption: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                active: false
             }],
             idList: "img-wrapper miniatures up down arrows",
-            classList: "img-lg info title caption lil-img active active-xs"
+            classList: "img-lg info title caption lil-img active active-xs",
+            currentSlide: 3,
         },
         methods: {
+            // showImg: function() {
+            //     if (active === true)
 
+            // }
         }
     })
 
